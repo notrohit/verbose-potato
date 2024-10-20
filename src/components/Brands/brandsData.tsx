48d556ba053e0359cc72a98fa17bbc4f610ae644
notrohit <rohitsingh250193@gmail.com>
2024-10-20 16:08:37 +0530

    add contacts

---
+++ src/components/Brands/brandsData.tsx
@@ -0,0 +1,48 @@
+import { Brand } from "@/types/brand";
+
+const brandsData: Brand[] = [
+  {
+    id: 6,
+    name: "teaspares",
+    href: "https://teaspares.com/",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 1,
+    name: "teaspares",
+    href: "https://teaspares.com/",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 2,
+    name: "teaspares",
+    href: "https://teaspares.com/",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 3,
+    name: "teaspares",
+    href: "https://teaspares.com/",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 4,
+    name: "teaspares",
+    href: "https://teaspares.com/",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 5,
+    name: "teaspares",
+    href: "https://teaspares.com/",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+];
+
+export default brandsData;
