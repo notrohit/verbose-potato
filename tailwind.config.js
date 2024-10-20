4931c533f38bbc7762a9d98efea06199ce9241bc
notrohit <rohitsingh250193@gmail.com>
2024-10-20 18:20:38 +0530

    uppdate assets and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
         white: "#FFFFFF",
         black: "#121723",
         dark: "#1D2430",
-        primary: "#4A6CF7",
+        primary: "#336633",
         yellow: "#FBB040",
         "bg-color-dark": "#171C28",
         "body-color": {
