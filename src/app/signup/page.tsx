b70cfbb1383973e45edf96e2b4cc49cba49363b7
notrohit <rohitsingh250193@gmail.com>
2024-10-20 16:17:13 +0530

    fix build error

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,7 +20,7 @@ const SignupPage = () => {
                   Create your account
                 </h3>
                 <p className="mb-11 text-center text-base font-medium text-body-color">
-                  title: "Teaspares", description: "Teaspares",
+                  test
                 </p>
                 <button className="border-stroke mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none">
                   <span className="mr-3">
